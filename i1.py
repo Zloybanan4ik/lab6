e2a185ac35e69033fd0164031df4adce293a9c95
Zloybanan4ik <denik55557777809@gmail.com>
2024-12-30 01:44:15 +0300

    individual

---
+++ i1.py
@@ -0,0 +1,10 @@
+sentence = input("Enter a sentence: ")
+
+matches = [word for word in sentence.split() if "нн" in word]
+
+if matches:
+    print("Words containing 'нн':")
+    for word in matches:
+        print(word)
+else:
+    print("No occurrences of 'нн' found.")
